908532de79f984b57d65542e713917bcbe18bf21
tomoki1604 <tomoki1604@proton.me>
2025-12-15 14:22:52 +0200

    Refactor: split logic into smaller helpers. Move inputs to their own folder.

--- day03/main.js
+++ day03/main.js
@@ -1,44 +1,58 @@
-import { input } from './input.js';
+import fs from 'fs';
 
-const testInput = [
-  '987654321111111',
-  '811111111111119',
-  '234234234234278',
-  '818181911112111',
-];
+const INPUT_FILES = {
+  input: './inputs/input.txt',
+  test: './inputs/test-input.txt',
+};
 const DIGITS = 12; // 2 digits for Part One, 12 for Part Two
 
-let totalJoltage = 0;
+const joltageRatings = readLines(INPUT_FILES.input);
 
-for (let item of input) {
-  let maxNumber = '';
+console.log('Total joltage = ', calculateTotalJoltage(joltageRatings));
 
+/* HELPERS */
+
+function readLines(filePath) {
+  return fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+}
+
+function calculateTotalJoltage(ratings) {
+  let totalJoltage = 0;
+
+  for (const rating of ratings) {
+    const maxNumber = getMaxNumber(rating);
+    totalJoltage += Number(maxNumber);
+  }
+
+  return totalJoltage;
+}
+
+function getMaxNumber(rating) {
+  let result = '';
   let startSlice = 0;
-  let endSlice = item.length - (DIGITS - 1);
+  let endSlice = rating.length - (DIGITS - 1);
 
-  while (maxNumber.length !== DIGITS) {
-    let slice = item.slice(startSlice, endSlice);
-    let [maxDigit, maxIndex] = findMaxDigit(slice);
+  while (result.length < DIGITS) {
+    const slice = rating.slice(startSlice, endSlice);
+    const [maxDigit, maxIndex] = findMaxDigit(slice);
 
-    maxNumber += maxDigit;
-
+    result += maxDigit;
     startSlice += maxIndex + 1;
     endSlice += 1;
   }
 
-  totalJoltage += parseInt(maxNumber);
-  console.log(maxNumber);
+  return result;
 }
 
-console.log('Total joltage = ', totalJoltage);
-
-function findMaxDigit(arr) {
+function findMaxDigit(str) {
   let maxDigit = null;
   let maxDigitIndex = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (parseInt(arr[i]) > maxDigit) {
-      maxDigit = parseInt(arr[i]);
+  for (let i = 0; i < str.length; i++) {
+    const digit = Number(str[i]);
+
+    if (digit > maxDigit) {
+      maxDigit = digit;
       maxDigitIndex = i;
     }
   }
